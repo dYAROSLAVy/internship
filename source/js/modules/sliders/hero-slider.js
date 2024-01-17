d424fca97504463f898ff5f23b7d71c9fa2c45cf
dYAROSLAVy <yadsp@yandex.ru>
2024-01-17 17:26:20 +0300

    Подготавливает сборку к работе и добавляет хедер

---
+++ source/js/modules/sliders/hero-slider.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper/bundle';
+
+const heroSlider = () => {
+  const swiperWrapper = document.querySelector('[data-hero-slider]');
+  const pagination = document.querySelector('[data-hero-slider-pagination]');
+
+  return new Swiper(swiperWrapper, {
+    loop: true,
+    autoHeight: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: true,
+    },
+    pagination: {
+      el: pagination,
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        allowTouchMove: true,
+      },
+    },
+  });
+};
+
+export {heroSlider};
